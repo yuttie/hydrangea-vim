02c0b43d4da1337ef6ce636cfca67bccfc27b065
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2015-10-03 15:59:19 +0900

    Bump the version to 0.2.2

--- night-theme.el
+++ night-theme.el
@@ -4,7 +4,7 @@
 
 ;; Author: Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
 ;; Keywords: theme, dark
-;; Version: 0.2.1
+;; Version: 0.2.2
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
