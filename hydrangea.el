df17b81ac645186ad942c83508d44540f95e9db9
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2015-10-09 09:30:14 +0900

    Refactoring

--- hydrangea.el
+++ hydrangea.el
@@ -60,80 +60,83 @@
 (defmacro hydrangea-define-theme (theme doc palette face-defs &rest body)
   ""
   (declare (indent defun) (debug t))
-  `(progn
-     (deftheme ,theme ,doc)
+  (cl-labels
+      ((theme-sym (&optional name)
+                  (intern (concat (symbol-name theme) "-theme" (if name (concat "-" name) nil)))))
+    `(progn
+       (deftheme ,theme ,doc)
 
-     (require 'color)
+       (require 'color)
 
-     (defgroup ,(intern (concat (symbol-name theme) "-theme")) nil
-       ""
-       :group 'customize)
+       (defgroup ,(theme-sym) nil
+         ""
+         :group 'customize)
 
-     (defcustom ,(intern (concat (symbol-name theme) "-theme-tune-hue")) 0
-       "Percentage of hue by which accent colors in the palette will be adjusted.
+       (defcustom ,(theme-sym "tune-hue") 0
+         "Percentage of hue by which accent colors in the palette will be adjusted.
 Both a positive value and a negative value are accepted."
-       :type 'number
-       :group ',(intern (concat (symbol-name theme) "-theme")))
+         :type 'number
+         :group ',(theme-sym))
 
-     (defcustom ,(intern (concat (symbol-name theme) "-theme-tune-saturation")) 0
-       "Percentage of saturation by which accent colors in the palette will be adjusted.
+       (defcustom ,(theme-sym "tune-saturation") 0
+         "Percentage of saturation by which accent colors in the palette will be adjusted.
 Both a positive value and a negative value are accepted.
 This value will be passed to the `color-saturate-name' function."
-       :type 'number
-       :group ',(intern (concat (symbol-name theme) "-theme")))
+         :type 'number
+         :group ',(theme-sym))
 
-     (defcustom ,(intern (concat (symbol-name theme) "-theme-tune-lightness")) 0
-       "Percentage of lightness by which accent colors in the palette will be adjusted.
+       (defcustom ,(theme-sym "tune-lightness") 0
+         "Percentage of lightness by which accent colors in the palette will be adjusted.
 Both a positive value and a negative value are accepted.
 This value will be passed to the `color-lighten-name' function."
-       :type 'number
-       :group ',(intern (concat (symbol-name theme) "-theme")))
+         :type 'number
+         :group ',(theme-sym))
 
-     (defun ,(intern (concat (symbol-name theme) "-theme-adjust")) (color-def)
-       (let* ((new-def (copy-sequence color-def))
-              (color (apply #'color-rgb-to-hsl (color-name-to-rgb (nth 1 color-def))))
-              (clamp-hue (lambda (value) (- value (floor value))))
-              (new-h (funcall clamp-hue (+ (nth 0 color) (/ ,(intern (concat (symbol-name theme) "-theme-tune-hue"))        100.0))))
-              (new-s (color-clamp       (+ (nth 1 color) (/ ,(intern (concat (symbol-name theme) "-theme-tune-saturation")) 100.0))))
-              (new-l (color-clamp       (+ (nth 2 color) (/ ,(intern (concat (symbol-name theme) "-theme-tune-lightness"))  100.0)))))
-         (setf (nth 1 new-def) (apply #'color-rgb-to-hex (apply #'color-hsl-to-rgb (list new-h new-s new-l))))
-         new-def))
+       (defun ,(theme-sym "adjust") (color-def)
+         (let* ((new-def (copy-sequence color-def))
+                (color (apply #'color-rgb-to-hsl (color-name-to-rgb (nth 1 color-def))))
+                (clamp-hue (lambda (value) (- value (floor value))))
+                (new-h (funcall clamp-hue (+ (nth 0 color) (/ ,(theme-sym "tune-hue")        100.0))))
+                (new-s (color-clamp       (+ (nth 1 color) (/ ,(theme-sym "tune-saturation") 100.0))))
+                (new-l (color-clamp       (+ (nth 2 color) (/ ,(theme-sym "tune-lightness")  100.0)))))
+           (setf (nth 1 new-def) (apply #'color-rgb-to-hex (apply #'color-hsl-to-rgb (list new-h new-s new-l))))
+           new-def))
 
-     (defcustom ,(intern (concat (symbol-name theme) "-theme-adjustment-mode-line"))
-       '(:eval (format ,(concat " " (capitalize (symbol-name theme)) "[H:%+d:S:%+d,L%+d]")
-                       ,(intern (concat (symbol-name theme) "-theme-tune-hue"))
-                       ,(intern (concat (symbol-name theme) "-theme-tune-saturation"))
-                       ,(intern (concat (symbol-name theme) "-theme-tune-lightness"))))
-       ""
-       :risky t
-       :group ',(intern (concat (symbol-name theme) "-theme")))
+       (defcustom ,(theme-sym "adjustment-mode-line")
+         '(:eval (format ,(concat " " (capitalize (symbol-name theme)) "[H:%+d:S:%+d,L%+d]")
+                         ,(theme-sym "tune-hue")
+                         ,(theme-sym "tune-saturation")
+                         ,(theme-sym "tune-lightness")))
+         ""
+         :risky t
+         :group ',(theme-sym))
 
-     (define-minor-mode ,(intern (concat (symbol-name theme) "-theme-adjustment-mode"))
-       "Provides key bindings for a user to adjust the saturation and the lightness of the theme easily."
-       :keymap (let ((map (make-sparse-keymap)))
-                 (define-key map (kbd "<f5>")  (lambda () (interactive) (load-theme ',theme)))
-                 (define-key map (kbd "<f6>")  (lambda ()
-                                                 (interactive)
-                                                 (setq ,(intern (concat (symbol-name theme) "-theme-tune-hue")) 0
-                                                       ,(intern (concat (symbol-name theme) "-theme-tune-saturation")) 0
-                                                       ,(intern (concat (symbol-name theme) "-theme-tune-lightness")) 0)
-                                                 (load-theme ',theme)))
-                 (define-key map (kbd "<f7>")  (lambda () (interactive) (setq ,(intern (concat (symbol-name theme) "-theme-tune-hue"))        (- ,(intern (concat (symbol-name theme) "-theme-tune-hue"))        5)) (load-theme ',theme)))
-                 (define-key map (kbd "<f8>")  (lambda () (interactive) (setq ,(intern (concat (symbol-name theme) "-theme-tune-hue"))        (+ ,(intern (concat (symbol-name theme) "-theme-tune-hue"))        5)) (load-theme ',theme)))
-                 (define-key map (kbd "<f9>")  (lambda () (interactive) (setq ,(intern (concat (symbol-name theme) "-theme-tune-saturation")) (- ,(intern (concat (symbol-name theme) "-theme-tune-saturation")) 5)) (load-theme ',theme)))
-                 (define-key map (kbd "<f10>") (lambda () (interactive) (setq ,(intern (concat (symbol-name theme) "-theme-tune-saturation")) (+ ,(intern (concat (symbol-name theme) "-theme-tune-saturation")) 5)) (load-theme ',theme)))
-                 (define-key map (kbd "<f11>") (lambda () (interactive) (setq ,(intern (concat (symbol-name theme) "-theme-tune-lightness"))  (- ,(intern (concat (symbol-name theme) "-theme-tune-lightness"))  5)) (load-theme ',theme)))
-                 (define-key map (kbd "<f12>") (lambda () (interactive) (setq ,(intern (concat (symbol-name theme) "-theme-tune-lightness"))  (+ ,(intern (concat (symbol-name theme) "-theme-tune-lightness"))  5)) (load-theme ',theme)))
-                 map)
-       :lighter ,(intern (concat (symbol-name theme) "-theme-adjustment-mode-line"))
-       :global)
+       (define-minor-mode ,(theme-sym "adjustment-mode")
+         "Provides key bindings for a user to adjust the saturation and the lightness of the theme easily."
+         :keymap (let ((map (make-sparse-keymap)))
+                   (define-key map (kbd "<f5>")  (lambda () (interactive) (load-theme ',theme)))
+                   (define-key map (kbd "<f6>")  (lambda ()
+                                                   (interactive)
+                                                   (setq ,(theme-sym "tune-hue") 0
+                                                         ,(theme-sym "tune-saturation") 0
+                                                         ,(theme-sym "tune-lightness") 0)
+                                                   (load-theme ',theme)))
+                   (define-key map (kbd "<f7>")  (lambda () (interactive) (setq ,(theme-sym "tune-hue")        (- ,(theme-sym "tune-hue")        5)) (load-theme ',theme)))
+                   (define-key map (kbd "<f8>")  (lambda () (interactive) (setq ,(theme-sym "tune-hue")        (+ ,(theme-sym "tune-hue")        5)) (load-theme ',theme)))
+                   (define-key map (kbd "<f9>")  (lambda () (interactive) (setq ,(theme-sym "tune-saturation") (- ,(theme-sym "tune-saturation") 5)) (load-theme ',theme)))
+                   (define-key map (kbd "<f10>") (lambda () (interactive) (setq ,(theme-sym "tune-saturation") (+ ,(theme-sym "tune-saturation") 5)) (load-theme ',theme)))
+                   (define-key map (kbd "<f11>") (lambda () (interactive) (setq ,(theme-sym "tune-lightness")  (- ,(theme-sym "tune-lightness")  5)) (load-theme ',theme)))
+                   (define-key map (kbd "<f12>") (lambda () (interactive) (setq ,(theme-sym "tune-lightness")  (+ ,(theme-sym "tune-lightness")  5)) (load-theme ',theme)))
+                   map)
+         :lighter ,(theme-sym "adjustment-mode-line")
+         :global)
 
-     (let ((palette (mapcar #',(intern (concat (symbol-name theme) "-theme-adjust")) ',(macroexpand palette))))
-       (custom-theme-set-faces ',theme
-                               ,@(mapcar (lambda (def) `(backquote (,(car def) ,(hydrangea-mkspec (macroexpand palette) (cdr def))))) (macroexpand face-defs)))
-       ,@body)
+       (let ((palette (mapcar #',(theme-sym "adjust") ',(macroexpand palette))))
+         (custom-theme-set-faces ',theme
+                                 ,@(mapcar (lambda (def) `(backquote (,(car def) ,(hydrangea-mkspec (macroexpand palette) (cdr def))))) (macroexpand face-defs)))
+         ,@body)
 
-     (provide-theme ',theme)))
+       (provide-theme ',theme))))
 
 
 (provide 'hydrangea)
