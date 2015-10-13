ff4b859b0f830f13455900ec564991c5dfc64059
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2015-10-13 14:08:39 +0900

    Change the color of constants to violet

--- hydrangea-theme.el
+++ hydrangea-theme.el
@@ -100,7 +100,7 @@
       (font-lock-builtin-face :foreground blue)
       (font-lock-comment-delimiter-face    :inherit (font-lock-comment-face))
       (font-lock-comment-face              :foreground unspecified :inherit (shadow))
-      (font-lock-constant-face             :foreground green)
+      (font-lock-constant-face             :foreground violet)
       (font-lock-doc-face                  :slant italic :inherit (shadow))
       (font-lock-function-name-face        :foreground yellow :slant italic)
       (font-lock-keyword-face              :foreground blue)
