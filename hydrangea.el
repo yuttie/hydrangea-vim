eadca0e3a7a073b15c150f9b31924a191786e036
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2015-10-22 11:06:04 +0900

    Bump the version of hydrangea.el to 1.0.0

--- hydrangea.el
+++ hydrangea.el
@@ -4,7 +4,7 @@
 
 ;; Author: Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
 ;; Keywords: theme, framework
-;; Version: 0.1.0
+;; Version: 1.0.0
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
