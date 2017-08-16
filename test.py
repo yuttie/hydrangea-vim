0ff2d8d72db7405fd00a6e2e02206e116e7ae03e
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2017-08-16 19:09:17 +0900

    Add the header string for output vim script

--- test.py
+++ test.py
@@ -1,9 +1,38 @@
+HEADER = """" Name:     hydrangea.vim --- Hydrangea theme for Vim
+" Author:   Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
+" URL:      https://github.com/yuttie/hydrangea-vim
+" Version:  3.0.0
+" License:  The MIT License (MIT)
+"     Copyright (c) 2015-2016 Yuta Taniguchi
+"
+"     Permission is hereby granted, free of charge, to any person obtaining a copy
+"     of this software and associated documentation files (the "Software"), to deal
+"     in the Software without restriction, including without limitation the rights
+"     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+"     copies of the Software, and to permit persons to whom the Software is
+"     furnished to do so, subject to the following conditions:
+"
+"     The above copyright notice and this permission notice shall be included in
+"     all copies or substantial portions of the Software.
+"
+"     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+"     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+"     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+"     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+"     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+"     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+"     THE SOFTWARE.
+"""
+
+
 try:
     import vim
 
     def execute(cmd):
         vim.command(cmd)
 except ImportError:
+    print(HEADER)
+
     def execute(cmd):
         print(cmd)
 
