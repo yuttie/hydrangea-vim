4327f81216f5ad6782b79193e8c96b759d5c0123
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2022-06-01 10:37:45 +0900

    Fix background color of 'Todo'

--- src/hydrangea.py
+++ src/hydrangea.py
@@ -89,7 +89,7 @@ color['Special']      = {'fg': blue2,            'bg': 'NONE',                'd
 color['Underlined']   = {'fg': 'fg',                                          'deco': 'underline'}
 color['Ignore']       = {'fg': 'bg'                                                              }
 color['Error']        = {'fg': red1,             'bg': red01,                 'deco': 'bold'     }
-color['Todo']         = {'fg': green1,                                        'deco': 'bold'     }
+color['Todo']         = {'fg': green1,           'bg': 'NONE',                'deco': 'bold'     }
 
 color['IncSearch']    = {'fg': base03,           'bg': magenta1,              'deco': 'bold'     }
 color['Search']       = {'fg': base03,           'bg': magenta1,              'deco': 'NONE'     }
