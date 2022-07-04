2dd70694f71a22da88db25ddb1c674b99c535170
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2022-07-04 12:19:11 +0900

    Update palette for lualine theme

--- lua/lualine/themes/hydrangea.lua
+++ lua/lualine/themes/hydrangea.lua
@@ -1,28 +1,33 @@
--- Palette (LCH values were measured in GIMP 2.10.4)
-local base03    = '#171c26'  -- L = 10, C =  8, H = 270
-local base02    = '#232833'  -- L = 16, C =  8, H = 270
-local base01    = '#303540'  -- L = 22, C =  8, H = 270
-local base00    = '#4b505d'  -- L = 28, C =  8, H = 270
-local base0     = '#465166'  -- L = 34, C = 14, H = 270
-local base1     = '#8791a9'  -- L = 60, C = 14, H = 270
-local base2     = '#cdd8f1'  -- L = 86, C = 14, H = 270
-local red01     = '#681c36'  -- L = ?, C = ?, H = ?
-local red1      = '#e91e63'  -- L = ?, C = ?, H = ?
-local green1    = '#98bf00'  -- L = ?, C = ?, H = ?
-local teal01    = '#013435'  -- L = ?, C = ?, H = ?
-local teal2     = '#019c9c'  -- L = ?, C = ?, H = ?
-local cyan01    = '#023342'  -- L = 19, C = 18, H = 232
-local cyan1     = '#1398bf'  -- L = 58, C = 38, H = 232
-local cyan3     = '#9bdffc'  -- L = 85, C = 27, H = 232
-local blue1     = '#3a69bf'  -- L = ?, C = ?, H = ?
-local blue2     = '#8baafe'  -- L = ?, C = ?, H = ?
-local blue3     = '#c9d4fd'  -- L = ?, C = ?, H = ?
-local violet1   = '#996ddb'  -- L = ?, C = ?, H = ?
-local violet2   = '#c398fe'  -- L = ?, C = ?, H = ?
-local violet3   = '#e2ccfe'  -- L = ?, C = ?, H = ?
-local magenta01 = '#491f38'  -- L = 19, C = 30, H = 343
-local magenta1  = '#c44597'  -- L = 50, C = 60, H = 343
-local magenta3  = '#ffc3e4'  -- L = 85, C = 27, H = 343
+-- Palette
+local base03    = '#1b202a'
+local base02    = '#232833'
+local base01    = '#303540'
+local base00    = '#4b505d'
+local base0     = '#465166'
+local base1     = '#96a1b9'
+local base2     = '#d3def7'
+local red01     = '#681c36'
+local red1      = '#e91e63'
+local green1    = '#98bf00'
+local green3    = '#dafd89'
+local teal01    = '#013435'
+local teal2     = '#019c9c'
+local cyan01    = '#023342'
+local cyan1     = '#0990b5'
+local cyan3     = '#9bdffc'
+local skyblue01 = '#002844'
+local skyblue1  = '#0487d8'
+local skyblue3  = '#73c5ff'
+local blue1     = '#466bb6'
+local blue2     = '#6d88ce'
+local violet01  = '#35264a'
+local violet0   = '#5b4182'
+local violet1   = '#996ddb'
+local violet2   = '#c398fe'
+local violet3   = '#e2ccfe'
+local magenta01 = '#4a1d45'
+local magenta1  = '#a6549c'
+local magenta3  = '#f7c5ef'
 
 return {
   normal = {
