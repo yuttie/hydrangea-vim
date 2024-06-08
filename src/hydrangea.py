9dec21a2030c1fdeb309c1d9466a7076590d106f
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2024-06-09 00:04:44 +0900

    Remove background from diagnostic messages and dim them

--- src/hydrangea.py
+++ src/hydrangea.py
@@ -278,10 +278,10 @@ color['@comment.note'] = 'Todo'
 # color['@tag.delimiter'] = ''
 
 # Diagnostic
-color['DiagnosticError'] = dict(fg=p['red']['01'],    bg=p['red']['05'],    deco='NONE')
-color['DiagnosticWarn']  = dict(fg=p['violet']['2'],  bg=p['violet']['0'],  deco='NONE')
-color['DiagnosticInfo']  = dict(fg=p['skyblue']['3'], bg=p['skyblue']['1'], deco='NONE')
-color['DiagnosticHint']  = dict(fg=p['green']['3'],   bg=p['green']['1'],   deco='NONE')
+color['DiagnosticError'] = dict(fg=p['red']['04'],     bg='NONE', deco='NONE')
+color['DiagnosticWarn']  = dict(fg=p['violet']['01'],  bg='NONE', deco='NONE')
+color['DiagnosticInfo']  = dict(fg=p['skyblue']['01'], bg='NONE', deco='NONE')
+color['DiagnosticHint']  = dict(fg=p['green']['01'],   bg='NONE', deco='NONE')
 
 # bufferline.nvim
 color['BufferLineFill']      = dict(bg=p['gray']['04'])
