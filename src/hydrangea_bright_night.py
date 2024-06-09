a5642031f8b6ce45b20f07ae8f2be060f6390588
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2024-06-09 22:13:13 +0900

    Add a new variation

---
+++ src/hydrangea_bright_night.py
@@ -0,0 +1,21 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+try:
+    import vim
+    # Assuming this file is executed with `:py3file %`
+    __file__ = vim.eval('expand("%")')
+except ImportError:
+    pass
+
+import os
+import sys
+sys.path.insert(0, os.path.dirname(__file__))
+
+from colorscheme import generate
+from hydrangea import generate_palette
+from hydrangea_night import define
+
+palette = generate_palette(32)
+color = define(palette)
+generate(color)
