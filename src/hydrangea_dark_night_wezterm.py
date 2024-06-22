4224d586e7942f7886c4de21154a26aee453712a
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2024-06-23 02:56:52 +0900

    Add color theme files for Wezterm

---
+++ src/hydrangea_dark_night_wezterm.py
@@ -0,0 +1,43 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+import os
+import sys
+sys.path.insert(0, os.path.dirname(__file__))
+
+import hydrangea
+
+p = hydrangea.generate_palette(20)
+
+print(f'''\
+# Hydrangea Dark Night
+[colors]
+foreground = "{p['gray'][+2][0]}"
+background = "{p['gray'][-3][0]}"
+cursor_bg = "{p['red'][-1][0]}"
+cursor_border = "{p['red'][-1][0]}"
+cursor_fg = "{p['gray'][-3][0]}"
+selection_bg = "{p['gray'][-1][0]}"
+selection_fg = "{p['gray'][-3][0]}"
+
+ansi = [
+    "{p['gray'  ][+0][0]}",
+    "{p['red'   ][+0][0]}",
+    "{p['green' ][+0][0]}",
+    "{p['teal'  ][+0][0]}",
+    "{p['blue'  ][+0][0]}",
+    "{p['violet'][+0][0]}",
+    "{p['cyan'  ][+0][0]}",
+    "{p['gray'  ][+0][0]}",
+]
+brights = [
+    "{p['gray'  ][+3][0]}",
+    "{p['red'   ][+3][0]}",
+    "{p['green' ][+3][0]}",
+    "{p['teal'  ][+3][0]}",
+    "{p['blue'  ][+3][0]}",
+    "{p['violet'][+3][0]}",
+    "{p['cyan'  ][+3][0]}",
+    "{p['gray'  ][+3][0]}",
+]
+''')
