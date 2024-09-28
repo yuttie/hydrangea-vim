d8f847287fd5dce5b8133ecb1da1978bbab612e1
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2024-09-28 21:58:04 +0900

    Use Oklab colorspace

--- src/colorspace.py
+++ src/colorspace.py
@@ -1,23 +1,41 @@
-from skimage.color import rgb2lab, lab2lch, lch2lab, lab2rgb
+import colour
+import numpy as np
 
-def hex2rgb(hex: str) -> tuple[float, float, float]:
+def hex2rgb(hex: str) -> np.ndarray:
     if hex[0] != '#':
         raise ValueError('Arg hex must start with "#"')
     r = int(hex[1:3], 16) / 0xff
     g = int(hex[3:5], 16) / 0xff
     b = int(hex[5:7], 16) / 0xff
-    return (r, g, b)
+    return np.array([r, g, b], dtype=np.float64)
 
-def rgb2hex(rgb: tuple[float, float, float]) -> str:
-    r, g, b = map(lambda x: int(0xff * x), rgb)
+def rgb2hex(rgb: np.ndarray) -> str:
+    r, g, b = map(lambda x: int(0xff * min(max(x, 0), 1)), rgb)
     return f'#{r:02x}{g:02x}{b:02x}'
 
-def hex2lch(hex: str) -> tuple[float, float, float]:
-    return tuple(lab2lch(rgb2lab(hex2rgb(hex), illuminant='D65')))
+def hex2oklab(hex: str) -> np.ndarray:
+    rgb = hex2rgb(hex)  # rgb: [0, 1], [0, 1], [0, 1]
+    xyz = colour.sRGB_to_XYZ(rgb)  # xyz: [0, 1], [0, 1], [0, 1]
+    oklab = colour.XYZ_to_Oklab(xyz)  # oklab: [0, 1], [-1, 1], [-1, 1]
+    return oklab
 
-def lch2hex(lch: tuple[float, float, float]) -> str:
-    return rgb2hex(lab2rgb(lch2lab(lch), illuminant='D65'))
+def oklab2hex(oklab: np.ndarray) -> str:  # oklab: [0, 1], [-1, 1], [-1, 1]
+    xyz = colour.Oklab_to_XYZ(oklab)  # xyz: [0, 1], [0, 1], [0, 1]
+    rgb = colour.XYZ_to_sRGB(xyz)  # rgb: [0, 1], [0, 1], [0, 1]
+    hex = rgb2hex(rgb)
+    return hex
 
-def adjust_lch(hex: str, delta_l: float, delta_c: float, delta_h: float) -> str:
-    l, c, h = hex2lch(hex)
-    return lch2hex((l + delta_l, c + delta_c, h + delta_h))
+def hex2oklch(hex: str) -> np.ndarray:
+    oklab = hex2oklab(hex)  # oklab: [0, 1], [-1, 1], [-1, 1]
+    oklch = colour.models.Jab_to_JCh(100 * oklab)  # oklch: [0, 100], [0, 100], [0, 360]
+    return oklch
+
+def oklch2hex(oklch: np.ndarray) -> str:  # oklch: [0, 100], [0, 100], [0, 360]
+    oklab = colour.models.JCh_to_Jab(oklch) / 100  # oklab: [0, 1], [-1, 1], [-1, 1]
+    hex = oklab2hex(oklab)
+    return hex
+
+def adjust_lightness(hex: str, delta) -> str:  # delta: [-100, 100]
+    oklab = hex2oklab(hex)  # oklab: [0, 1], [-1, 1], [-1, 1]
+    oklab[0] += delta / 100
+    return oklab2hex(oklab)
--- src/hydrangea.py
+++ src/hydrangea.py
@@ -1,6 +1,7 @@
 # This file is intended to be imported and used only by hydrangea_*.py that
 # appropriately setup sys.path for this module
-from colorspace import lch2hex, adjust_lch
+from math import pi
+from colorspace import oklch2hex, adjust_lightness
 
 def generate_palette(base_lightness: float):
     """Generate a palette.
@@ -34,37 +35,37 @@ def generate_palette(base_lightness: float):
     # Base colors
     # L: [0, 100]
     # C: [0, 100]
-    # H: [0, 2 * PI]
+    # H: [0, 360]
     base_color = dict()
-    base_color['gray']        = lch2hex((base_lightness,  7.554, 4.888))
-    base_color['red']         = lch2hex((base_lightness, 76.168, 6.100))
-    base_color['yellowgreen'] = lch2hex((base_lightness, 33.038, 1.972))
-    base_color['green']       = lch2hex((base_lightness, 33.038, 2.269))
-    base_color['teal']        = lch2hex((base_lightness, 34.680, 3.427))
-    base_color['cyan']        = lch2hex((base_lightness, 22.086, 4.135))
-    base_color['skyblue']     = lch2hex((base_lightness, 24.056, 4.701))
-    base_color['blue']        = lch2hex((base_lightness, 31.778, 4.930))
-    base_color['violet']      = lch2hex((base_lightness, 57.453, 5.386))
-    base_color['magenta']     = lch2hex((base_lightness, 32.716, 5.769))
+    base_color['gray']        = oklch2hex((base_lightness,  7.554, 360 * 4.888 / 2 * pi))
+    base_color['red']         = oklch2hex((base_lightness, 76.168, 360 * 6.100 / 2 * pi))
+    base_color['yellowgreen'] = oklch2hex((base_lightness, 33.038, 360 * 1.972 / 2 * pi))
+    base_color['green']       = oklch2hex((base_lightness, 33.038, 360 * 2.269 / 2 * pi))
+    base_color['teal']        = oklch2hex((base_lightness, 34.680, 360 * 3.427 / 2 * pi))
+    base_color['cyan']        = oklch2hex((base_lightness, 22.086, 360 * 4.135 / 2 * pi))
+    base_color['skyblue']     = oklch2hex((base_lightness, 24.056, 360 * 4.701 / 2 * pi))
+    base_color['blue']        = oklch2hex((base_lightness, 31.778, 360 * 4.930 / 2 * pi))
+    base_color['violet']      = oklch2hex((base_lightness, 57.453, 360 * 5.386 / 2 * pi))
+    base_color['magenta']     = oklch2hex((base_lightness, 32.716, 360 * 5.769 / 2 * pi))
 
     palette = dict()
     for cname in color_names:
         if cname in palette:
             continue
         palette[cname] = {
-            -6: (adjust_lch(base_color[cname], -30, 0, 0),  53),
-            -5: (adjust_lch(base_color[cname], -28, 0, 0),  53),
-            -4: (adjust_lch(base_color[cname], -24, 0, 0),  53),
-            -3: (adjust_lch(base_color[cname], -20, 0, 0),  53),
-            -2: (adjust_lch(base_color[cname], -16, 0, 0), 133),
-            -1: (adjust_lch(base_color[cname],  -8, 0, 0), 133),
-            0:  (adjust_lch(base_color[cname],   0, 0, 0), 133),
-            +1: (adjust_lch(base_color[cname],   8, 0, 0), 133),
-            +2: (adjust_lch(base_color[cname],  16, 0, 0), 225),
-            +3: (adjust_lch(base_color[cname],  24, 0, 0), 225),
-            +4: (adjust_lch(base_color[cname],  32, 0, 0), 225),
-            +5: (adjust_lch(base_color[cname],  40, 0, 0), 225),
-            +6: (adjust_lch(base_color[cname],  48, 0, 0), 225),
+            -6: (adjust_lightness(base_color[cname], -30),  53),
+            -5: (adjust_lightness(base_color[cname], -28),  53),
+            -4: (adjust_lightness(base_color[cname], -24),  53),
+            -3: (adjust_lightness(base_color[cname], -20),  53),
+            -2: (adjust_lightness(base_color[cname], -16), 133),
+            -1: (adjust_lightness(base_color[cname],  -8), 133),
+            0:  (adjust_lightness(base_color[cname],   0), 133),
+            +1: (adjust_lightness(base_color[cname],   8), 133),
+            +2: (adjust_lightness(base_color[cname],  16), 225),
+            +3: (adjust_lightness(base_color[cname],  24), 225),
+            +4: (adjust_lightness(base_color[cname],  32), 225),
+            +5: (adjust_lightness(base_color[cname],  40), 225),
+            +6: (adjust_lightness(base_color[cname],  48), 225),
         }
 
     return palette
